73c437bb474dda375b0c8c1ec1aef38d3c58d12a
YUNJIJI <fyes38916401@gmail.com>
2025-12-04 12:18:33 +0800

    add test and fix code

--- server/src/music_gen/test.py
+++ server/src/music_gen/test.py
@@ -1,10 +1,13 @@
 import os
-import numpy as np
-from scipy.io import wavfile
-from music_generator import MusicGenerator
 import time
 
-#音樂儲存位置
+import numpy as np
+from scipy.io import wavfile
+
+from .music_generator import MusicGenerator
+
+
+# 音樂儲存位置
 def store_music(music, filename="song.wav"):
     output_dir = os.path.join(os.getcwd(), "output")
     os.makedirs(output_dir, exist_ok=True)
@@ -20,15 +23,14 @@ def store_music(music, filename="song.wav"):
     print(f"Saved music to {output_path}")
     return output_path
 
+
 if __name__ == "__main__":
     mg = MusicGenerator()
     # for emotion in mg.emotion_prompt:
-    start = time.time()
-    emotion = "angry"
-    mg.emotion_to_prompt(emotion)
-    print("generating...")
-    print(f"Emotion = {emotion}")
-    music = mg.generate_music(prompt= mg.prompt)
-    end = time.time()
-    store_music(music, filename=f"{emotion}.wav")
-    print(f"生成耗時: {end - start:.2f} 秒")
+    for i in range(2):
+        start = time.time()
+        emotion = "angry"
+        music = mg.generate(emotion=emotion, duration=5)
+        end = time.time()
+        store_music(music, filename=f"{emotion}.wav")
+        print(f"生成耗時: {end - start:.2f} 秒")
